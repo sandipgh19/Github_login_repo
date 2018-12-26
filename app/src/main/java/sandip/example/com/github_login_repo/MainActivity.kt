791174f4935464983799c6c34b21c3c1f66ac1b4
sandipghosh <sandipgh19@gmail.com>
2018-12-26 22:14:21 +0530

    Initial commit

---
+++ app/src/main/java/sandip/example/com/github_login_repo/MainActivity.kt
@@ -0,0 +1,12 @@
+package sandip.example.com.github_login_repo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
