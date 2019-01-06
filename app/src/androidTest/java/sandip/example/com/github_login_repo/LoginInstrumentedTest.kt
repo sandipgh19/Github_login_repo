28dd324a7a5102c88dfee971b67025962b48d33c
sandipghosh <sandipgh19@gmail.com>
2019-01-06 12:48:38 +0530

    setup espresso

---
+++ app/src/androidTest/java/sandip/example/com/github_login_repo/LoginInstrumentedTest.kt
@@ -0,0 +1,15 @@
+package sandip.example.com.github_login_repo
+
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginInstrumentedTest {
+
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+
+    }
+
+}
