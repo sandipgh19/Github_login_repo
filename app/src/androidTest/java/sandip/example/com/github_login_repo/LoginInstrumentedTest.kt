fbfa40c2bb7727b93fa61ba79c86589ce7e38ad7
sandipghosh <sandipgh19@gmail.com>
2019-01-07 22:12:49 +0530

    some test cases for login and activity

--- app/src/androidTest/java/sandip/example/com/github_login_repo/LoginInstrumentedTest.kt
+++ app/src/androidTest/java/sandip/example/com/github_login_repo/LoginInstrumentedTest.kt
@@ -1,15 +1,48 @@
 package sandip.example.com.github_login_repo
 
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LoginInstrumentedTest {
 
-    @Test
-    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+    private val username = "chike"
+    private val password = "password"
 
+    @Rule
+    @JvmField
+    var activityRule = ActivityTestRule<MainActivity>(
+        MainActivity::class.java
+    )
+
+    @Test
+    @Throws(Exception::class)
+    fun clickLoginButton_opensLoginUi() {
+        onView(withId(R.id.authenticate)).perform(click())
     }
+
+//    @Test
+//    fun shouldApplyCorrectTitle() {
+//        toolbarWithTitle(R.string.login) check isDisplayed
+//    }
+//
+//    @Test
+//    fun shouldContainUsernameInput() {
+//        R.id.username check isDisplayed
+//    }
+//
+//    @Test
+//    fun usernameInputShouldApplyCorrectHint() {
+//        R.id.username hasHint R.string.s
+//    }
+
+
+
 
 }
