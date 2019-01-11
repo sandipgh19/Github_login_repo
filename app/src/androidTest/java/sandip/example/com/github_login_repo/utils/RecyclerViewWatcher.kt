1f537d210ef5ce527fd30a77d2553f51be70bf48
sandipghosh <sandipgh19@gmail.com>
2019-01-11 21:28:32 +0530

    case for recycler view and view model

---
+++ app/src/androidTest/java/sandip/example/com/github_login_repo/utils/RecyclerViewWatcher.kt
@@ -0,0 +1,57 @@
+package sandip.example.com.github_login_repo.utils
+
+import android.content.res.Resources
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+class RecyclerViewWatcher(private val viewId: Int) {
+
+    fun atPosition(position: Int): Matcher<View> {
+        return atPositionOnView(position, -1)
+    }
+
+    fun atPositionOnView(position: Int, targetViewId: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            var resources: Resources? = null
+            var childView: View? = null
+
+            override fun describeTo(description: Description) {
+                var idDescription = Integer.toString(viewId)
+                if (this.resources != null) {
+                    idDescription = try {
+                        this.resources!!.getResourceName(viewId)
+                    } catch (var4: Resources.NotFoundException) {
+                        "$viewId (resource name not found)"
+                    }
+                }
+
+                description.appendText("RecyclerView with id: $idDescription at position: $position")
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+
+                this.resources = view.resources
+
+                if (childView == null) {
+                    val recyclerView = view.rootView.findViewById<RecyclerView>(viewId)
+                    if (recyclerView?.id == viewId) {
+                        val viewHolder = recyclerView.findViewHolderForAdapterPosition(position)
+                        childView = viewHolder?.itemView
+                    } else {
+                        return false
+                    }
+                }
+
+                return if (targetViewId == -1) {
+                    view === childView
+                } else {
+                    val targetView = childView?.findViewById<View>(targetViewId)
+                    view === targetView
+                }
+            }
+        }
+    }
+}
