efa37ba78ae4b77f30a55650032ae4fbec951d2b
sandipghosh <sandipgh19@gmail.com>
2019-03-03 18:01:23 +0530

    bug fixes

---
+++ app/src/androidTest/java/sandip/example/com/github_login_repo/utils/MockitoExt.kt
@@ -0,0 +1,11 @@
+package sandip.example.com.github_login_repo.utils
+
+import org.mockito.ArgumentCaptor
+import org.mockito.Mockito
+
+/**
+ * a kotlin friendly mock that handles generics
+ */
+inline fun <reified T> mock(): T = Mockito.mock(T::class.java)
+
+inline fun <reified T> argumentCaptor(): ArgumentCaptor<T> = ArgumentCaptor.forClass(T::class.java)
